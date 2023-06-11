103de19e1ec2a98f8063855f6e513ea026393d3c
Xiang Zheng <68246040+archmyth@users.noreply.github.com>
2023-06-11 18:41:23 +0000

    fixed appjs

--- src/js/app.js
+++ src/js/app.js
@@ -15,8 +15,8 @@ import "../style/index.css";
         linkedin: null,
         instagram: null,
 
-        name: null,
-        lastname: null,
+        name: name,
+        lastname: last name,
         role: null,
         country: null,
         city: null
@@ -56,19 +56,19 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://randomuser.me/api/portraits/men/44.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: "archmyth",
     linkedin: null,
     instagram: null,
-    name: null,
-    lastname: null,
-    role: null,
-    country: null,
-    city: null
+    name: "name",
+    lastname: "last name",
+    role: "role",
+    country: "USA",
+    city: "Madison"
   };
   render(window.variables); //render the card for the first time
 
